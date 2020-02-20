e15358b938343fadc4fcc89a8014f6b58d2632f3
Danyels0310 <32487560+Danyels0310@users.noreply.github.com>
2020-02-20 20:13:12 +0100

    Update comaltacat.php

--- comaltacat.php
+++ comaltacat.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-<h1>ALTA CATEGORÍAS - Nombre del alumno</h1>
+<h1>ALTA CATEGORÍAS - Daniel Fernández</h1>
 <?php
 include "conexion.php";
 /* Se muestra el formulario la primera vez */
