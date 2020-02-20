e3465a702c00f226ed84d249d14ed863b1f4af71
Danyels0310 <32487560+Danyels0310@users.noreply.github.com>
2020-02-20 20:07:38 +0100

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 /* Conexión BD */
 define('DB_SERVER', '10.129.14.139');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'rootroot');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'COMPRASWEB'); 
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
